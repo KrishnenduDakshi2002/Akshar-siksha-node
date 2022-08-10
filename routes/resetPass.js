2d416df1decf72b4b52d36308ac1c62fb459ea98
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-08-10 17:03:09 +0530

    Fixed bug for resetPass.js

--- routes/resetPass.js
+++ routes/resetPass.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const jwt = require('jsonwebtoken');
-// VERIFY TOKEN 
-const verify = require('../verifyToken');
 
-router.get('/:token',verify,async (req,res)=>{
+
+router.get('/:token',async (req,res)=>{
     // console.log(req.params.id);
     try {
-        const userId = req.user._id;
+        const token = req.params.token;
+        const userId = jwt.verify(token,process.env.TOKEN_SECRET); // outputs the user _id
         const user = await User.findOne({_id:userId});
         if(user) return res.render('resetPass',{"user_id":req.params.id});
         

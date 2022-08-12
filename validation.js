28087e1cb8a1cd8a58c70272c4d35e432b9cdb1b
krishnendu Dakshi <kdakshi2020@gamil.com>
2022-08-12 19:03:22 +0530

    Fixed bug in validation

--- validation.js
+++ validation.js
@@ -5,16 +5,16 @@ const joi = require('joi');
 const registerValidation = (data)=>{
 
     const schema = joi.object({
-        first_name:joi.string().min(6).required(),
-        last_name:joi.string().min(6).required(),
+        first_name:joi.string().min(1).required(),
+        last_name:joi.string().min(1).required(),
         role: joi.string().required(),
         email: joi.string().min(6).max(255).required().email(),
         phoneNumber : joi.string().min(10).max(10).required(),
         address: joi.string().min(2).max(1024).required(),
-        institute: joi.string().min(5).max(255),
+        institute: joi.string().min(2).max(255),
         password: joi.string().min(6).max(1024).required(),
         password2: joi.ref('password'),
-        institute_code: joi.string()
+        institute_code: joi.string().required()
 
     });
 
